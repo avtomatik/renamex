5dbb61c2df9b39590ca568ec08a9b940ec4fb18a
Alexander Mikhailov <extroper@yandex.ru>
2026-02-05 22:11:13 +0300

    docs(FileService): add docstrings for class and methods
    - Added detailed docstrings for the FileService class.
    - Documented all methods including list_files, list_all_file_names, list_empty_files, and walk_directory.
    - Included usage examples and parameter descriptions.

--- tools/filters.py
+++ tools/filters.py
@@ -90,92 +90,6 @@ class EmptyFileFilter:
         )
 
 
-class FileService:
-    """Service to list and filter files in a directory."""
-
-    def __init__(self, folder: Path = Path(".")):
-        self.folder = folder
-
-    def list_files(
-        self,
-        filters: list[MatchFileFilter] | None = None,
-        lowercase: bool = False,
-    ) -> list[str]:
-        """_summary_
-
-        Args:
-            filters (List[FileFilter], optional): _description_.
-                Defaults to None.
-            lowercase (bool, optional): _description_. Defaults to False.
-
-        Returns:
-            List[str]: _description_
-
-        Usage:
-            >>> service = FileService(path)
-            >>> files = service.list_files(lowercase=True)
-        """
-        filters = filters or []
-        result: list[str] = []
-        for file in self.folder.iterdir():
-            if file.is_file() and all(f.matches(file) for f in filters):
-                result.append(file.name.lower() if lowercase else file.name)
-        return result
-
-    def list_all_file_names(self) -> list[str]:
-        """_summary_
-
-        Returns:
-            List[str]: _description_
-
-        Usage:
-            >>> service = FileService(path)
-            >>> source_file_names = service.list_all_file_names()
-        """
-        return [file.name for file in self.folder.iterdir() if file.is_file()]
-
-    def list_empty_files(self, reserved: set[str]) -> list[str]:
-        """_summary_
-
-        Args:
-            reserved (Set[str]): _description_
-
-        Returns:
-            List[str]: _description_
-
-        Usage:
-            >>> service = FileService(path)
-            >>> empty_files = service.list_empty_files(reserved)
-        """
-        return [
-            file.name
-            for file in self.folder.iterdir()
-            if (
-                file.is_file()
-                and file.stat().st_size == 0
-                and file.name not in reserved
-            )
-        ]
-
-    def walk_directory(self) -> list[tuple[list[str], list[str]]]:
-        """_summary_
-
-        Returns:
-            List[Tuple[List[str], List[str]]]: _description_
-
-        Usage:
-            >>> service = FileService(path)
-            >>> structure = service.walk_directory()
-        """
-        result: list[tuple[list[str], list[str]]] = []
-        for path in self.folder.rglob("*"):
-            if path.is_dir():
-                dirnames = [p.name for p in path.iterdir() if p.is_dir()]
-                filenames = [p.name for p in path.iterdir() if p.is_file()]
-                result.append((dirnames, filenames))
-        return result
-
-
 class NullFileFilter:
     """Accepts all regular files (ignores directories)."""
 
@@ -193,3 +107,145 @@ class FileExtensionFilter:
 
     def is_target(self, file: Path) -> bool:
         return file.is_file() and file.suffix in self.extensions
+
+
+class FileService:
+    """
+    A service class for managing and filtering files in a directory.
+
+    This class provides functionality to list, filter, and walk through files
+    in a specified directory, with various filtering options. It supports
+    filters based on file names, patterns, file size, and more.
+
+    Attributes:
+        folder (Path): The directory path where files will be listed and
+        filtered.
+
+    Methods:
+        list_files(
+            filters: list[MatchFileFilter] | None = None,
+            lowercase: bool = False,
+        ) -> list[str]:
+            Lists all files in the directory, applying any specified filters.
+
+        list_all_file_names() -> list[str]:
+            Lists all file names in the directory, without any filtering.
+
+        list_empty_files(reserved: set[str]) -> list[str]:
+            Lists all empty files in the directory, excluding reserved file
+            names.
+
+        walk_directory() -> list[tuple[list[str], list[str]]]:
+            Recursively walks through the directory and lists files and
+            subdirectories.
+    """
+
+    def __init__(self, folder: Path = Path(".")):
+        """
+        Initializes the FileService with the given folder path.
+
+        Args:
+            folder (Path): The directory path to search for files
+            (default is current directory).
+        """
+        self.folder = folder
+
+    def list_files(
+        self,
+        filters: list[MatchFileFilter] | None = None,
+        lowercase: bool = False,
+    ) -> list[str]:
+        """
+        Lists files in the directory, optionally applying filters and
+        transforming file names.
+
+        Args:
+            filters (list[MatchFileFilter] | None): A list of filters to apply
+            to the files.
+                If None, no filters are applied.
+            lowercase (bool): If True, all file names are converted to
+            lowercase.
+
+        Returns:
+            list[str]: A list of file names that pass the applied filters.
+
+        Usage:
+            >>> service = FileService(Path("/path/to/dir"))
+            >>> files = service.list_files(
+                    filters=[NameExclusionFilter(names_to_exclude)],
+                    lowercase=True,
+                )
+        """
+        filters = filters or []
+        result: list[str] = []
+        for file in self.folder.iterdir():
+            if file.is_file() and all(f.matches(file) for f in filters):
+                result.append(file.name.lower() if lowercase else file.name)
+        return result
+
+    def list_all_file_names(self) -> list[str]:
+        """
+        Lists all file names in the directory without applying any filters.
+
+        Returns:
+            list[str]: A list of all file names in the directory.
+
+        Usage:
+            >>> service = FileService(Path("/path/to/dir"))
+            >>> all_file_names = service.list_all_file_names()
+        """
+        return [file.name for file in self.folder.iterdir() if file.is_file()]
+
+    def list_empty_files(self, reserved: set[str]) -> list[str]:
+        """
+        Lists all empty files in the directory, excluding those that are
+        reserved.
+
+        Args:
+            reserved (set[str]): A set of reserved file names to exclude from
+            the result.
+
+        Returns:
+            list[str]: A list of empty file names that are not reserved.
+
+        Usage:
+            >>> service = FileService(Path("/path/to/dir"))
+            >>> empty_files = service.list_empty_files(reserved={"README.md"})
+        """
+        return [
+            file.name
+            for file in self.folder.iterdir()
+            if (
+                file.is_file()
+                and file.stat().st_size == 0
+                and file.name not in reserved
+            )
+        ]
+
+    def walk_directory(self) -> list[tuple[list[str], list[str]]]:
+        """
+        Recursively walks through the directory and lists subdirectories and
+        files.
+
+        This method returns a list of tuples where each tuple contains:
+            - A list of subdirectories in the current directory.
+            - A list of files in the current directory.
+
+
+        Returns:
+            list[tuple[list[str], list[str]]]: A list of tuples representing
+                                                subdirectories and files found
+                                                while walking through the
+                                                directory.
+
+        Usage:
+            >>> service = FileService(Path("/path/to/dir"))
+            >>> directory_structure = service.walk_directory()
+        """
+        result: list[tuple[list[str], list[str]]] = []
+        for path in self.folder.rglob("*"):
+            if path.is_dir():
+                dirnames = [p.name for p in path.iterdir() if p.is_dir()]
+                filenames = [p.name for p in path.iterdir() if p.is_file()]
+                result.append((dirnames, filenames))
+        return result
